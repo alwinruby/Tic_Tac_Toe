019463cc660a4dd5b75f2a323fe14513a55f7c40
asolanky <alwinsolanky@gmail.com>
2018-08-03 07:16:09 +0100

    initialised states

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 Five parts to game
 1. Initial states
 2. Anonymous function
-
 */
 window.onload = function functionName() {
-
+  var num;
+  var box;
+  var ctx;
+  var turn = 1;
+  var filled;
+  var symbol;
+  var winner;
+  var gameOver = false;
 }
