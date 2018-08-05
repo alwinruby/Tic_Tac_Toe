4f583a7bb44a22b2389cc8101c5ba5a6d88018f4
asolanky <alwinsolanky@gmail.com>
2018-08-05 14:33:02 +0100

    win conditions

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ window.onload = function functionName() {
   var symbol;
   var winner;
   var gameOver = false;
+  filled = [false, false, false, false, false, false, false, false, false];
+  symbol = ['', '', '', '', '', '', '', '', '',];
+  winner = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  //winner[0] = [0, 1, 2]
+  //winner[1] = [3, 4, 5]
+  //winner[0][0] = 0
+  //winner[0][1] = 1
+  //winner[0][2] = 2
 }
