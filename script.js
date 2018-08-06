5e9cf38b72e957874287ebf14d4de36596a6e700
asolanky <alwinsolanky@gmail.com>
2018-08-06 12:02:43 +0100

    new game button refresh

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Five parts to game
 1. Initial states
 2. Anonymous function
 */
-window.onload = function functionName() {
+window.onload = function() {
   var num;
   var box;
   var ctx;
@@ -15,9 +15,13 @@ window.onload = function functionName() {
   filled = [false, false, false, false, false, false, false, false, false];
   symbol = ['', '', '', '', '', '', '', '', '',];
   winner = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-  //winner[0] = [0, 1, 2]
-  //winner[1] = [3, 4, 5]
-  //winner[0][0] = 0
-  //winner[0][1] = 1
-  //winner[0][2] = 2
+
+  //2. New Game button
+  var n = document.getElementById("new");
+  n.addEventListener("click", newGame);
+
+  //newGame function
+  function newGame(){
+    document.location.reload();
+  }
 }
